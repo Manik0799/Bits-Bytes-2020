184180134226136bea4a68f1403497de8d15aacf
Manik Gupta <dungeon0799master@gmai.com>
2020-10-19 23:40:24 +0530

    Trying error

--- src/components/Section/Categories.js
+++ src/components/Section/Categories.js
@@ -1,95 +1,162 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import { Card, CardImg, CardImgOverlay, Row, Col, Container } from "reactstrap";
 import Fade from "react-reveal/Fade";
 import { Link } from "react-router-dom";
 
+import firebase from "../../firebase.js";
+
+// Importing card Data from firebase
+// import Categorylist from "./fetchingCardLogic"
+
+
 // Importing the data of categories
-import data from "../../categoryData";
 
-function Categories() {
-  const [expand, setExpand] = useState(false);
+
+
+
+ function Categories()
+{
+  const [categorylist,setcategorylist]=useState([]);
+  const [displayCards,setDisplayCards]=useState(false);
+
+    // Function to retreive data from firebase
+    const fetchFunction = () => {
+      let Categorylist = []
+         // Fetching data from Image Storage for Card images
+        var storage=firebase.storage();
+        firebase.database().ref("Category").on("value", snapshot => {
+
+          snapshot.forEach((snap) => {
+            
+            const temp_url = "";
+            // then -> returns a Promise
+            storage.ref().child(`Category/${snap.val().link}/Card.jpg`).getDownloadURL().then((url)=>{
+
+              temp_url = url;
+           
+            }).catch((error)=>{
+              console.log(error);
+            })
+
+               Categorylist.push({
+                imageurl:temp_url,
+                link:snap.val().link,
+                title:snap.val().title,
+              });
+          
+        });
+        
+
+      });
+        
+        return Categorylist
+    }
+  
+   useEffect(()=>{
+  
+      setcategorylist(fetchFunction)
+      setDisplayCards(true);
+
+      console.log('====================================');
+      console.log(displayCards);
+      console.log("Now displaying the list data")
+      console.log(categorylist);
+      console.log(categorylist.length)
+
+      console.log('====================================');
+  },[displayCards])
+
+
+    return (
+      <>
+      
+        <div
+        className="section section-dark text-center"
+        style={{
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Container>
+          <Fade top>
+            <h1 style={{ color: "#fff", marginBottom: "2rem", marginTop: 0 }}>
+              Categories
+            </h1>
+          </Fade>
+
+          {/* Rendering all the cards */}
+
+            {displayCards ? <h1>{categorylist.length}</h1> : <h1>"Loading categories"</h1>}
+
+          {/* {displayCards ? <Row className="justify-content-md-center">
+
+                {categorylist.map((data, key) => {
+                    
+                
+                        return (
+                          <Col lg="4" md="6" sm="2" className="ml-auto mr-auto">
+                            <div
+                              style={{
+                                margin: "0.5%",
+                                width: "100%",
+                                height: "100%",
+                                objectFit: "none",
+                                alignItems: "center",
+                              }}
+                            >
+
+                                <Fade left>
+                                {console.log("entered"), console.log(data)}
+                                
+                              <div key={key}>
+                                <Link to={data.link}>
+                                  <Card>
+                                    <CardImg
+                                      src={data.imageurl}
+                                      style={{
+                                        width: "100%",
+                                        height: 300,
+                                        borderRadius: 8,
+                                      }}
+                                    ></CardImg>
+                                
+                                    <CardImgOverlay
+                                      style={{
+                                        color: "#fff",
+                                        textDecorationStyle: "double",
+                                        alignContent: "flex-end",
+                                        objectFit: "cover",
+                                        margin: 0,
+                                        backgroundColor: "rgba(0,0,0,0.35)",
+                                      }}
+                                    >
+                                    
+                                      <h2 style={{ fontWeight: "400" }}>{data.title}</h2>
+                                    </CardImgOverlay>
+                                  </Card>
+                                </Link>
+                              </div>
+                              </Fade>
+                            </div>
+                          </Col>
+                        );
+            })} </Row> 
+            
+             : <h1>"Loading Cards"</h1>
+          }
+            
+          */}
+        </Container>
+      </div>
+      
+      
+      
+      </>
+    )
  
-
-
-
-
-  const showAllCards = () => {
-    setExpand(!expand);
-  };
-
-  return (
-    <div
-      className="section section-dark text-center"
-      style={{
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <Container>
-        <Fade top>
-          <h1 style={{ color: "#fff", marginBottom: "2rem", marginTop: 0 }}>
-            Categories
-          </h1>
-        </Fade>
-
-        <Row className="justify-content-md-center">
-          {data.map((data, key) => {
-            {
-              /* Displaying a single card from the data array */
-            }
-            return (
-              <Col lg="4" md="6" sm="2" className="ml-auto mr-auto">
-                <div
-                  style={{
-                    margin: "0.5%",
-                    width: "100%",
-                    height: "100%",
-                    objectFit: "none",
-                    alignItems: "center",
-                  }}
-                >
-                  {/* <Fade left> */}
-                  <div key={key}>
-                    <Link to={data.link}>
-                      <Card>
-                        <CardImg
-                          src={data.image}
-                          style={{
-                            width: "100%",
-                            height: 300,
-                            borderRadius: 8,
-                          }}
-                        ></CardImg>
-                        {/* <CardBody>
-                        <CardText >{data.name}</CardText>
-                      </CardBody> */}
-                        <CardImgOverlay
-                          style={{
-                            color: "#fff",
-                            textDecorationStyle: "double",
-                            alignContent: "flex-end",
-                            objectFit: "cover",
-                            margin: 0,
-                            backgroundColor: "rgba(0,0,0,0.35)",
-                          }}
-                        >
-                          {/* <CardText>{data.name}</CardText>
-                           */}
-                          <h2 style={{ fontWeight: "400" }}>{data.name}</h2>
-                        </CardImgOverlay>
-                      </Card>
-                    </Link>
-                  </div>
-                  {/* </Fade> */}
-                </div>
-              </Col>
-            );
-          })}
-        </Row>
-      </Container>
-    </div>
-  );
 }
 
-export default Categories;
+
+
+export default  Categories;
--- src/firebase.js
+++ src/firebase.js
@@ -5,14 +5,14 @@ const settings = {timestampsInSnapshots: true};
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const config = {
-    // apiKey: "AIzaSyDOTQyJXGJoHhMXwesi2CSFGNMRMqIK0YY",
-    // authDomain: "blogbitsandbytes.firebaseapp.com",
-    // databaseURL: "https://blogbitsandbytes.firebaseio.com",
-    // projectId: "blogbitsandbytes",
-    // storageBucket: "blogbitsandbytes.appspot.com",
-    // messagingSenderId: "501709774452",
-    // appId: "1:501709774452:web:04e90f6fa1731807fabb21",
-    // measurementId: "G-EQT3ST6KT4"
+    apiKey: "AIzaSyDOTQyJXGJoHhMXwesi2CSFGNMRMqIK0YY",
+    authDomain: "blogbitsandbytes.firebaseapp.com",
+    databaseURL: "https://blogbitsandbytes.firebaseio.com",
+    projectId: "blogbitsandbytes",
+    storageBucket: "blogbitsandbytes.appspot.com",
+    messagingSenderId: "501709774452",
+    appId: "1:501709774452:web:04e90f6fa1731807fabb21",
+    measurementId: "G-EQT3ST6KT4"
   };
 
 
