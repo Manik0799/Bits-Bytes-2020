f0feb4e9a276ccf76f4147001fbae6dba19fff95
nikhil <nikhilchachan111@gmail.com>
2020-10-19 23:46:32 +0530

    edits

--- src/components/Section/Categories.js
+++ src/components/Section/Categories.js
@@ -6,43 +6,69 @@ import { Link } from "react-router-dom";
 
 import firebase from "../../firebase.js";
 
+// Importing card Data from firebase
+// import Categorylist from "./fetchingCardLogic"
+
+
 // Importing the data of categories
+
+
+
 
  function Categories()
 {
   const [categorylist,setcategorylist]=useState([]);
-  const [loading,setloading]=useState(true);
+  const [displayCards,setDisplayCards]=useState(false);
 
+    // Function to retreive data from firebase
+    const fetchFunction = () => {
+      let Categorylist = []
+         // Fetching data from Image Storage for Card images
+        var storage=firebase.storage();
+        firebase.database().ref("Category").on("value", snapshot => {
+
+          snapshot.forEach((snap) => {
+            
+            const temp_url = "";
+            // then -> returns a Promise
+            storage.ref().child(`Category/${snap.val().link}/Card.jpg`).getDownloadURL().then((url)=>{
+
+              temp_url = url;
+           
+            }).catch((error)=>{
+              console.log(error);
+            })
+
+               Categorylist.push({
+                imageurl:temp_url,
+                link:snap.val().link,
+                title:snap.val().title,
+              });
+          
+        });
+        
+
+      });
+        
+        return Categorylist
+    }
+  
    useEffect(()=>{
   
-      let Categorylist = [];
-      var storage=firebase.storage();
-        firebase.database().ref("Category").on("value", snapshot => {
-        snapshot.forEach((snap) => {
-       
-          storage.ref().child(`Category/${snap.val().link}/Card.jpg`).getDownloadURL().then((url)=>{
-            var object={
-              imageurl:url,
-              link:snap.val().link,
-              title:snap.val().title,
-            }
-          Categorylist.push(object);
-           }).catch((error)=>{
-             console.log(error);
-           })
-         
-      });
-      });
-      setcategorylist(Categorylist);
-      setloading(false);
+      setcategorylist(fetchFunction)
+      setDisplayCards(true);
+
       console.log('====================================');
-      console.log(loading);
+      console.log(displayCards);
+      console.log("Now displaying the list data")
       console.log(categorylist);
+      console.log(categorylist.length)
+
       console.log('====================================');
-  },[loading])
+  },[displayCards])
+
 
     return (
-   
       <>
       
         <div
@@ -58,66 +84,67 @@ import firebase from "../../firebase.js";
               Categories
             </h1>
           </Fade>
-      <h1>{categorylist.length}</h1>
-       {loading?console.log("Loading"): <Row className="justify-content-md-center">
-            {categorylist.map((data, key) => {
-              {
-                /* Displaying a single card from the data array */
-              }
-              return (
-                <Col lg="4" md="6" sm="2" className="ml-auto mr-auto">
-                  <div
-                    style={{
-                      margin: "0.5%",
-                      width: "100%",
-                      height: "100%",
-                      objectFit: "none",
-                      alignItems: "center",
-                    }}
-                  >
 
-                                      <Fade left>
-                      {console.log("entered"), console.log(data)}
-                      
-                    <div key={key}>
-                      <Link to={data.link}>
-                        <Card>
-                          <CardImg
-                            src={data.imageurl}
-                            style={{
-                              width: "100%",
-                              height: 300,
-                              borderRadius: 8,
-                            }}
-                          ></CardImg>
-                       
-                          <CardImgOverlay
-                            style={{
-                              color: "#fff",
-                              textDecorationStyle: "double",
-                              alignContent: "flex-end",
-                              objectFit: "cover",
-                              margin: 0,
-                              backgroundColor: "rgba(0,0,0,0.35)",
-                            }}
-                          >
-                          
-                            <h2 style={{ fontWeight: "400" }}>{data.title}</h2>
-                          </CardImgOverlay>
-                        </Card>
-                      </Link>
-                    </div>
-                    </Fade>
-                  </div>
-                </Col>
-              );
-            })}
-          </Row>
-          
-          
-          
+          {/* Rendering all the cards */}
+
+            {displayCards ? <h1>{categorylist.length}</h1> : <h1>"Loading categories"</h1>}
+
+          {/* {displayCards ? <Row className="justify-content-md-center">
+                {categorylist.map((data, key) => {
+                    
+                
+                        return (
+                          <Col lg="4" md="6" sm="2" className="ml-auto mr-auto">
+                            <div
+                              style={{
+                                margin: "0.5%",
+                                width: "100%",
+                                height: "100%",
+                                objectFit: "none",
+                                alignItems: "center",
+                              }}
+                            >
+                                <Fade left>
+                                {console.log("entered"), console.log(data)}
+                                
+                              <div key={key}>
+                                <Link to={data.link}>
+                                  <Card>
+                                    <CardImg
+                                      src={data.imageurl}
+                                      style={{
+                                        width: "100%",
+                                        height: 300,
+                                        borderRadius: 8,
+                                      }}
+                                    ></CardImg>
+                                
+                                    <CardImgOverlay
+                                      style={{
+                                        color: "#fff",
+                                        textDecorationStyle: "double",
+                                        alignContent: "flex-end",
+                                        objectFit: "cover",
+                                        margin: 0,
+                                        backgroundColor: "rgba(0,0,0,0.35)",
+                                      }}
+                                    >
+                                    
+                                      <h2 style={{ fontWeight: "400" }}>{data.title}</h2>
+                                    </CardImgOverlay>
+                                  </Card>
+                                </Link>
+                              </div>
+                              </Fade>
+                            </div>
+                          </Col>
+                        );
+            })} </Row> 
+            
+             : <h1>"Loading Cards"</h1>
           }
-         
+            
+          */}
         </Container>
       </div>
       
