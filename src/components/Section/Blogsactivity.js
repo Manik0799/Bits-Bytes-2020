99c60d2bdd697fef25ce557728ba0ad328ea59a2
Manik Gupta <dungeon0799master@gmai.com>
2020-10-24 22:36:01 +0530

    Magazine Display page and modal left

--- src/components/Section/Blogsactivity.js
+++ src/components/Section/Blogsactivity.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 
 import {
   Card,
@@ -13,70 +13,54 @@ import {
   Badge,
 } from "reactstrap";
 
-function Blogsactivity({ blogsData }) {
+import Modal from "./Modal"
+
+function Blogsactivity({ blogsData, setModalData }) {
  
   const arrayOfObj = Object.values(blogsData);
-  var array=[];
-  function func(value)
-  {
-    return (!value);
-  }
+  // var array=[];
+  // function func(value)
+  // {
+  //   return (!value);
+  // }
+
+  // const handleClick = (blogText, blogImages) => {
+  //   console.log(blogText, blogImages)
+
+      
+  // }
+
+
+
   return (
    
     <>
       <Row className="justify-content-md-center">
+
         {arrayOfObj.map((data, key) => {
-          {array.push(false)}
+          {/* {array.push(false)} */}
           return (
           
             <div key={key}>
               
               {/* {data.represent=="hide"? */}
               <Col lg="4" md="6" sm="6" className="justify-content-md-center">
-                <Card style={{ width: "20rem" }}>
-                  <CardImgOverlay
-                    style={{
-                      textAlign: "left",
-                      // textAlignLast: "end",
-                      // textOrientation: "sideways",
-                    }}
-                  >
-                    <Badge
-                      style={{
-                        backgroundColor: "#fff",
-                        color: "#000",
-                        borderTopRightRadius: 5,
-                      }}
-                    >
-                      {data.category}
-                    </Badge>
-                  </CardImgOverlay>
-                  <div style={{
-                    width:"300",
-                    height:"500"
-                  }}>
-                  <CardImg top src={data.blogimage} alt={data.category}
-                   />
-                   </div>
 
-                  <CardBody>
-                    <CardTitle style={{ fontWeight: 400, fontSize: 16 }}>
-                      {data.title}
-                    </CardTitle>
-                    <CardText style={{ textAlign: "left", marginTop: "12px" }}>
-                      {/* {{var obj=JSON.parse(data.text)}} */}
-                      {data.text.substr(0, 40)}
-                      &nbsp;.....
-                    </CardText>
-                    <CardText className="text-left">
-                      <small className="text-muted">{data.date}</small>
-                    </CardText>
-                    <Button color="primary" onClick={func(array[key])}
-                    >
-                      { array[key] ? "Read more" : "Read less"}
-                    </Button>
-                  </CardBody>
-                </Card>
+              {/* Individual Card */}
+                      <div class="card" style={{width: "20rem"}}>
+                      {data.blogimages.image1 ?<img className="card-img-top" src = {data.blogimages.image1} alt="card-image" /> : null}
+                        
+                        <div className="card-body">
+                          <h4 className="card-title">{data.title}</h4>
+                          <p className="card-text">{data.text.substr(0,70) } &nbsp;....</p>
+                          <p className="card-text">{data.date }</p>
+                          <a  className="btn btn-primary"
+                            onClick = {() => setModalData(data)}
+                          >Read More</a>
+                        </div>
+                      </div>
+
+
               </Col>
             </div>
           );
---
+++ src/components/Section/Modal.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Modal({data}) {
+    console.log(data.text, data.blogimages)
+    return(
+        <div>
+        </div>
+    )
+}
+
+export default Modal
